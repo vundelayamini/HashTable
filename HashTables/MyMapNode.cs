593d8ef43cd93b68a107e71bb26371c016343476
vundelayamini <vundelayamini@gmail.com>
2021-05-10 16:01:59 +0530

    [yamini]Add: Ability to find frequency of words

---
+++ HashTables/MyMapNode.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTables
+{
+
+    public class MyMapNode<K, V>// MyMapNode class with key value
+    {
+        internal readonly int size;
+        //And creating linked list which take key and value
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        //Adding the elements
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        //find frequency of word
+        Dictionary<string, int> frequency = new Dictionary<string, int>();
+        public void Frequency(MyMapNode<int, string> hash)
+        {
+           
+            for (int key = 0; key < hash.size; key++)
+            {
+                frequency.TryAdd(hash.Get(key).ToLower(), 0);
+                frequency[hash.Get(key).ToLower()]++;
+            }
+            foreach (KeyValuePair<string, int> item in frequency)
+            {
+                Console.WriteLine($"frequency of word '{item.Key}' is {item.Value}");
+            }
+        }
+        //Deletion using key
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+        //Get the key value
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        //Get arraypossition
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+    public struct KeyValue<k, v>
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+}
+
--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -6,7 +6,19 @@ namespace HashTables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to HashTable program: ");
+            Console.WriteLine("Welcome to HashTable program");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
+            hash.Add(0, "To");
+            hash.Add(1, "be");
+            hash.Add(2, "or");
+            hash.Add(3, "not");
+            hash.Add(4, "to");
+            hash.Add(5, "be");
+            hash.Frequency(hash);
+
+
         }
     }
 }
+
+   
